73ca1998820fae75569ecc8832d25835e612e98a
fra <fra@fra.dev>
2021-01-07 13:07:13 +0100

    box width & align

--- modules/template/skins/posts-template.php
+++ modules/template/skins/posts-template.php
@@ -228,174 +228,202 @@ class Posts_Template extends Base_Skin {
 
         $this->end_controls_section();
     }
-    
+
     public function register_additional_design_controls() {
-		$this->start_controls_section(
-			'section_design_box',
-			[
-				'label' => __( 'Box', 'elementor-pro' ),
-				'tab' => Controls_Manager::TAB_STYLE,
-			]
-		);
-                
-                $this->add_control(
-			'box_element_width',
-			[
-				'label' => __( 'Box Width', 'elementor-pro' ),
-				'type' => Controls_Manager::SLIDER,
-				'size_units' => [ 'px', '%' ],
-				'range' => [
-					'%' => [
-						'min' => 0,
-						'max' => 100,
-					],
-				],
-                                'default' => [
-					'unit' => '%',
-					'size' => 100,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .elementor-post > .elementor' => 'width: {{SIZE}}{{UNIT}}',
-				],
-			]
-		);
+        $this->start_controls_section(
+                'section_design_box',
+                [
+                    'label' => __('Box', 'elementor-pro'),
+                    'tab' => Controls_Manager::TAB_STYLE,
+                ]
+        );
 
-		$this->add_control(
-			'box_border_width',
-			[
-				'label' => __( 'Border Width', 'elementor-pro' ),
-				'type' => Controls_Manager::DIMENSIONS,
-				'size_units' => [ 'px' ],
-				'range' => [
-					'px' => [
-						'min' => 0,
-						'max' => 50,
-					],
-				],
-				'selectors' => [
-					'{{WRAPPER}} .elementor-post' => 'border-style: solid; border-width: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}}',
-				],
-			]
-		);
+        $this->add_responsive_control(
+                'box_element_width',
+                [
+                    'label' => __('Box Width', 'elementor-pro'),
+                    'type' => Controls_Manager::SLIDER,
+                    'size_units' => ['%'],
+                    'range' => [
+                        '%' => [
+                            'min' => 0,
+                            'max' => 100,
+                        ],
+                    ],
+                    'default' => [
+                        'unit' => '%',
+                        'size' => 100,
+                    ],
+                    'selectors' => [
+                        '{{WRAPPER}} .elementor-post > .elementor' => 'width: {{SIZE}}{{UNIT}}',
+                    ],
+                ]
+        );
 
-		$this->add_control(
-			'box_border_radius',
-			[
-				'label' => __( 'Border Radius', 'elementor-pro' ),
-				'type' => Controls_Manager::SLIDER,
-				'size_units' => [ 'px', '%' ],
-				'range' => [
-					'px' => [
-						'min' => 0,
-						'max' => 200,
-					],
-				],
-				'selectors' => [
-					'{{WRAPPER}} .elementor-post' => 'border-radius: {{SIZE}}{{UNIT}}',
-				],
-			]
-		);
+        $this->add_control(
+                'box_element_alignment',
+                [
+                    'label' => __('Alignment', 'elementor-pro'),
+                    'type' => Controls_Manager::CHOOSE,
+                    'options' => [
+                        'flex-start' => [
+                            'title' => __('Left', 'elementor-pro'),
+                            'icon' => 'eicon-text-align-left',
+                        ],
+                        'center' => [
+                            'title' => __('Center', 'elementor-pro'),
+                            'icon' => 'eicon-text-align-center',
+                        ],
+                        'flex-end' => [
+                            'title' => __('Right', 'elementor-pro'),
+                            'icon' => 'eicon-text-align-right',
+                        ],
+                    ],
+                    'condition' => [                        
+                        $this->get_control_id('box_element_width[size]!') => 100,
+                    ],
+                    'selectors' => [
+                        '{{WRAPPER}} .elementor-post' => 'justify-content: {{VALUE}}',
+                    ],
+                ]
+        );
 
-		$this->add_control(
-			'box_padding',
-			[
-				'label' => __( 'Padding', 'elementor-pro' ),
-				'type' => Controls_Manager::DIMENSIONS,
-				'size_units' => [ 'px' ],
-				'range' => [
-					'px' => [
-						'min' => 0,
-						'max' => 50,
-					],
-				],
-				'selectors' => [
-					'{{WRAPPER}} .elementor-post' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}}',
-				],
-			]
-		);
+        $this->add_control(
+                'box_border_width',
+                [
+                    'label' => __('Border Width', 'elementor-pro'),
+                    'type' => Controls_Manager::DIMENSIONS,
+                    'size_units' => ['px'],
+                    'range' => [
+                        'px' => [
+                            'min' => 0,
+                            'max' => 50,
+                        ],
+                    ],
+                    'selectors' => [
+                        '{{WRAPPER}} .elementor-post' => 'border-style: solid; border-width: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}}',
+                    ],
+                ]
+        );
 
-		$this->start_controls_tabs( 'bg_effects_tabs' );
+        $this->add_control(
+                'box_border_radius',
+                [
+                    'label' => __('Border Radius', 'elementor-pro'),
+                    'type' => Controls_Manager::SLIDER,
+                    'size_units' => ['px', '%'],
+                    'range' => [
+                        'px' => [
+                            'min' => 0,
+                            'max' => 200,
+                        ],
+                    ],
+                    'selectors' => [
+                        '{{WRAPPER}} .elementor-post' => 'border-radius: {{SIZE}}{{UNIT}}',
+                    ],
+                ]
+        );
 
-		$this->start_controls_tab( 'classic_style_normal',
-			[
-				'label' => __( 'Normal', 'elementor-pro' ),
-			]
-		);
+        $this->add_control(
+                'box_padding',
+                [
+                    'label' => __('Padding', 'elementor-pro'),
+                    'type' => Controls_Manager::DIMENSIONS,
+                    'size_units' => ['px'],
+                    'range' => [
+                        'px' => [
+                            'min' => 0,
+                            'max' => 50,
+                        ],
+                    ],
+                    'selectors' => [
+                        '{{WRAPPER}} .elementor-post' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}}',
+                    ],
+                ]
+        );
 
-		$this->add_group_control(
-			Group_Control_Box_Shadow::get_type(),
-			[
-				'name' => 'box_shadow',
-				'selector' => '{{WRAPPER}} .elementor-post',
-			]
-		);
+        $this->start_controls_tabs('bg_effects_tabs');
 
-		$this->add_control(
-			'box_bg_color',
-			[
-				'label' => __( 'Background Color', 'elementor-pro' ),
-				'type' => Controls_Manager::COLOR,
-				'selectors' => [
-					'{{WRAPPER}} .elementor-post' => 'background-color: {{VALUE}}',
-				],
-			]
-		);
+        $this->start_controls_tab('classic_style_normal',
+                [
+                    'label' => __('Normal', 'elementor-pro'),
+                ]
+        );
 
-		$this->add_control(
-			'box_border_color',
-			[
-				'label' => __( 'Border Color', 'elementor-pro' ),
-				'type' => Controls_Manager::COLOR,
-				'selectors' => [
-					'{{WRAPPER}} .elementor-post' => 'border-color: {{VALUE}}',
-				],
-			]
-		);
+        $this->add_group_control(
+                Group_Control_Box_Shadow::get_type(),
+                [
+                    'name' => 'box_shadow',
+                    'selector' => '{{WRAPPER}} .elementor-post',
+                ]
+        );
 
-		$this->end_controls_tab();
+        $this->add_control(
+                'box_bg_color',
+                [
+                    'label' => __('Background Color', 'elementor-pro'),
+                    'type' => Controls_Manager::COLOR,
+                    'selectors' => [
+                        '{{WRAPPER}} .elementor-post' => 'background-color: {{VALUE}}',
+                    ],
+                ]
+        );
 
-		$this->start_controls_tab( 'classic_style_hover',
-			[
-				'label' => __( 'Hover', 'elementor-pro' ),
-			]
-		);
+        $this->add_control(
+                'box_border_color',
+                [
+                    'label' => __('Border Color', 'elementor-pro'),
+                    'type' => Controls_Manager::COLOR,
+                    'selectors' => [
+                        '{{WRAPPER}} .elementor-post' => 'border-color: {{VALUE}}',
+                    ],
+                ]
+        );
 
-		$this->add_group_control(
-			Group_Control_Box_Shadow::get_type(),
-			[
-				'name' => 'box_shadow_hover',
-				'selector' => '{{WRAPPER}} .elementor-post:hover',
-			]
-		);
+        $this->end_controls_tab();
 
-		$this->add_control(
-			'box_bg_color_hover',
-			[
-				'label' => __( 'Background Color', 'elementor-pro' ),
-				'type' => Controls_Manager::COLOR,
-				'selectors' => [
-					'{{WRAPPER}} .elementor-post:hover' => 'background-color: {{VALUE}}',
-				],
-			]
-		);
+        $this->start_controls_tab('classic_style_hover',
+                [
+                    'label' => __('Hover', 'elementor-pro'),
+                ]
+        );
 
-		$this->add_control(
-			'box_border_color_hover',
-			[
-				'label' => __( 'Border Color', 'elementor-pro' ),
-				'type' => Controls_Manager::COLOR,
-				'selectors' => [
-					'{{WRAPPER}} .elementor-post:hover' => 'border-color: {{VALUE}}',
-				],
-			]
-		);
+        $this->add_group_control(
+                Group_Control_Box_Shadow::get_type(),
+                [
+                    'name' => 'box_shadow_hover',
+                    'selector' => '{{WRAPPER}} .elementor-post:hover',
+                ]
+        );
 
-		$this->end_controls_tab();
+        $this->add_control(
+                'box_bg_color_hover',
+                [
+                    'label' => __('Background Color', 'elementor-pro'),
+                    'type' => Controls_Manager::COLOR,
+                    'selectors' => [
+                        '{{WRAPPER}} .elementor-post:hover' => 'background-color: {{VALUE}}',
+                    ],
+                ]
+        );
 
-		$this->end_controls_tabs();
+        $this->add_control(
+                'box_border_color_hover',
+                [
+                    'label' => __('Border Color', 'elementor-pro'),
+                    'type' => Controls_Manager::COLOR,
+                    'selectors' => [
+                        '{{WRAPPER}} .elementor-post:hover' => 'border-color: {{VALUE}}',
+                    ],
+                ]
+        );
 
-		$this->end_controls_section();
-	}
+        $this->end_controls_tab();
+
+        $this->end_controls_tabs();
+
+        $this->end_controls_section();
+    }
 
     public function render() {
         $this->parent->query_posts();
@@ -505,14 +533,13 @@ class Posts_Template extends Base_Skin {
         }
         ?>
         <nav class="elementor-pagination" role="navigation" aria-label="<?php esc_attr_e('Pagination', 'elementor-pro'); ?>">
-        <?php echo implode(PHP_EOL, $links); ?>
+            <?php echo implode(PHP_EOL, $links); ?>
         </nav>
-            <?php
-        }
-
-        public function get_container_class() {
-            return 'elementor-posts--skin-' . $this->get_id();
-        }
-
+        <?php
     }
-    
+
+    public function get_container_class() {
+        return 'elementor-posts--skin-' . $this->get_id();
+    }
+
+}
